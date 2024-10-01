b10c06a1e0072386c0a470e72a815b760e5524c4
Elliot Janvier <elliot.janvier@epitech.eu>
2024-09-30 21:30:46 -0400

    [EDIT] test CI

--- delivery-app-frontend/pages/_document.tsx
+++ delivery-app-frontend/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en" style={{ backgroundColor: '#F1F8F2' }} >
+    <Html lang="en" style={{ backgroundColor: "#F1F8F2" }}>
       <Head />
       <body className="antialiased">
         <Main />
