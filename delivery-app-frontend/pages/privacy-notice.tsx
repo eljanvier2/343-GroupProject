d3d5654d64e6ae882fe3e7f1468b5e29350c1466
DarkMed15 <darkmedtv@gmail.com>
2024-10-01 01:34:13 -0400

    [add] lower footer 3 pages

---
+++ delivery-app-frontend/pages/privacy-notice.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+
+const PrivacyNotice = (): JSX.Element => {
+  return (
+    <div className="w-full px-10 py-12">
+      <div className="flex justify-center mb-10">
+      </div>
+
+      <h1 className="text-header1 font-bold text-center text-customBlack uppercase mb-8">Privacy Notice</h1>
+
+      <div className="text-customBlack space-y-10 text-body leading-relaxed">
+        
+        <section>
+          <h2 className="text-header3 font-semibold mb-4">Introduction</h2>
+          <p>
+            We are committed to protecting your privacy. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu ultricies lectus, id dictum ex. Etiam tempus bibendum ligula, et dignissim nulla placerat ut. Mauris finibus dignissim risus a blandit. Mauris efficitur nisi at augue facilisis blandit. Duis rhoncus ligula vitae dolor aliquet aliquam ut euismod felis. Pellentesque cursus nibh lorem, ullamcorper vestibulum dolor porta nec. Praesent felis neque, egestas ut volutpat quis, condimentum quis mi.
+            Pellentesque egestas enim vel ante ultricies pulvinar. Duis nec dignissim augue. Ut lobortis viverra turpis. Nullam hendrerit blandit augue ut pulvinar. Aliquam gravida non ipsum sed venenatis. Curabitur maximus augue nibh, a iaculis augue venenatis eget. Duis pretium quis odio vel molestie. Sed luctus tellus leo, sed maximus massa volutpat eu.
+            Nullam consectetur ullamcorper nisi et pellentesque. Quisque vel leo non turpis lacinia venenatis non sodales dolor. Phasellus congue mauris quam, eget luctus velit suscipit eget. Nam a dictum dolor, ut commodo leo. Duis augue quam, facilisis ut arcu vel, ullamcorper commodo metus. Etiam malesuada erat massa, sit amet ornare nulla fringilla et. Nam eget velit congue, viverra dolor ac, lacinia justo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis feugiat arcu, non congue ex. Integer vel mollis leo.
+
+          </p>
+        </section>
+
+        <section>
+          <h2 className="text-header3 font-semibold mb-4">Information We Collect</h2>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu ultricies lectus, id dictum ex. Etiam tempus bibendum ligula, et dignissim nulla placerat ut. Mauris finibus dignissim risus a blandit. Mauris efficitur nisi at augue facilisis blandit. Duis rhoncus ligula vitae dolor aliquet aliquam ut euismod felis. Pellentesque cursus nibh lorem, ullamcorper vestibulum dolor porta nec. Praesent felis neque, egestas ut volutpat quis, condimentum quis mi.
+            Pellentesque egestas enim vel ante ultricies pulvinar. Duis nec dignissim augue. Ut lobortis viverra turpis. Nullam hendrerit blandit augue ut pulvinar. Aliquam gravida non ipsum sed venenatis. Curabitur maximus augue nibh, a iaculis augue venenatis eget. Duis pretium quis odio vel molestie. Sed luctus tellus leo, sed maximus massa volutpat eu.
+            Nullam consectetur ullamcorper nisi et pellentesque. Quisque vel leo non turpis lacinia venenatis non sodales dolor. Phasellus congue mauris quam, eget luctus velit suscipit eget. Nam a dictum dolor, ut commodo leo. Duis augue quam, facilisis ut arcu vel, ullamcorper commodo metus. Etiam malesuada erat massa, sit amet ornare nulla fringilla et. Nam eget velit congue, viverra dolor ac, lacinia justo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis feugiat arcu, non congue ex. Integer vel mollis leo.
+
+          </p>
+        </section>
+
+        <section>
+          <h2 className="text-header3 font-semibold mb-4">How We Use Your Information</h2>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu ultricies lectus, id dictum ex. Etiam tempus bibendum ligula, et dignissim nulla placerat ut. Mauris finibus dignissim risus a blandit. Mauris efficitur nisi at augue facilisis blandit. Duis rhoncus ligula vitae dolor aliquet aliquam ut euismod felis. Pellentesque cursus nibh lorem, ullamcorper vestibulum dolor porta nec. Praesent felis neque, egestas ut volutpat quis, condimentum quis mi.
+            Pellentesque egestas enim vel ante ultricies pulvinar. Duis nec dignissim augue. Ut lobortis viverra turpis. Nullam hendrerit blandit augue ut pulvinar. Aliquam gravida non ipsum sed venenatis. Curabitur maximus augue nibh, a iaculis augue venenatis eget. Duis pretium quis odio vel molestie. Sed luctus tellus leo, sed maximus massa volutpat eu.
+            Nullam consectetur ullamcorper nisi et pellentesque. Quisque vel leo non turpis lacinia venenatis non sodales dolor. Phasellus congue mauris quam, eget luctus velit suscipit eget. Nam a dictum dolor, ut commodo leo. Duis augue quam, facilisis ut arcu vel, ullamcorper commodo metus. Etiam malesuada erat massa, sit amet ornare nulla fringilla et. Nam eget velit congue, viverra dolor ac, lacinia justo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis feugiat arcu, non congue ex. Integer vel mollis leo.
+
+          </p>
+
+        </section>
+
+        <section>
+          <h2 className="text-header3 font-semibold mb-4">Data Sharing and Disclosure</h2>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu ultricies lectus, id dictum ex. Etiam tempus bibendum ligula, et dignissim nulla placerat ut. Mauris finibus dignissim risus a blandit. Mauris efficitur nisi at augue facilisis blandit. Duis rhoncus ligula vitae dolor aliquet aliquam ut euismod felis. Pellentesque cursus nibh lorem, ullamcorper vestibulum dolor porta nec. Praesent felis neque, egestas ut volutpat quis, condimentum quis mi.
+            Pellentesque egestas enim vel ante ultricies pulvinar. Duis nec dignissim augue. Ut lobortis viverra turpis. Nullam hendrerit blandit augue ut pulvinar. Aliquam gravida non ipsum sed venenatis. Curabitur maximus augue nibh, a iaculis augue venenatis eget. Duis pretium quis odio vel molestie. Sed luctus tellus leo, sed maximus massa volutpat eu.
+            Nullam consectetur ullamcorper nisi et pellentesque. Quisque vel leo non turpis lacinia venenatis non sodales dolor. Phasellus congue mauris quam, eget luctus velit suscipit eget. Nam a dictum dolor, ut commodo leo. Duis augue quam, facilisis ut arcu vel, ullamcorper commodo metus. Etiam malesuada erat massa, sit amet ornare nulla fringilla et. Nam eget velit congue, viverra dolor ac, lacinia justo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis feugiat arcu, non congue ex. Integer vel mollis leo.
+
+          </p>
+        </section>
+
+        <section>
+          <h2 className="text-header3 font-semibold mb-4">Data Security</h2>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu ultricies lectus, id dictum ex. Etiam tempus bibendum ligula, et dignissim nulla placerat ut. Mauris finibus dignissim risus a blandit. Mauris efficitur nisi at augue facilisis blandit. Duis rhoncus ligula vitae dolor aliquet aliquam ut euismod felis. Pellentesque cursus nibh lorem, ullamcorper vestibulum dolor porta nec. Praesent felis neque, egestas ut volutpat quis, condimentum quis mi.
+            Pellentesque egestas enim vel ante ultricies pulvinar. Duis nec dignissim augue. Ut lobortis viverra turpis. Nullam hendrerit blandit augue ut pulvinar. Aliquam gravida non ipsum sed venenatis. Curabitur maximus augue nibh, a iaculis augue venenatis eget. Duis pretium quis odio vel molestie. Sed luctus tellus leo, sed maximus massa volutpat eu.
+            Nullam consectetur ullamcorper nisi et pellentesque. Quisque vel leo non turpis lacinia venenatis non sodales dolor. Phasellus congue mauris quam, eget luctus velit suscipit eget. Nam a dictum dolor, ut commodo leo. Duis augue quam, facilisis ut arcu vel, ullamcorper commodo metus. Etiam malesuada erat massa, sit amet ornare nulla fringilla et. Nam eget velit congue, viverra dolor ac, lacinia justo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis feugiat arcu, non congue ex. Integer vel mollis leo.
+
+          </p>
+        </section>
+
+        <section>
+          <h2 className="text-header3 font-semibold mb-4">Your Rights</h2>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu ultricies lectus, id dictum ex. Etiam tempus bibendum ligula, et dignissim nulla placerat ut. Mauris finibus dignissim risus a blandit. Mauris efficitur nisi at augue facilisis blandit. Duis rhoncus ligula vitae dolor aliquet aliquam ut euismod felis. Pellentesque cursus nibh lorem, ullamcorper vestibulum dolor porta nec. Praesent felis neque, egestas ut volutpat quis, condimentum quis mi.
+            Pellentesque egestas enim vel ante ultricies pulvinar. Duis nec dignissim augue. Ut lobortis viverra turpis. Nullam hendrerit blandit augue ut pulvinar. Aliquam gravida non ipsum sed venenatis. Curabitur maximus augue nibh, a iaculis augue venenatis eget. Duis pretium quis odio vel molestie. Sed luctus tellus leo, sed maximus massa volutpat eu.
+            Nullam consectetur ullamcorper nisi et pellentesque. Quisque vel leo non turpis lacinia venenatis non sodales dolor. Phasellus congue mauris quam, eget luctus velit suscipit eget. Nam a dictum dolor, ut commodo leo. Duis augue quam, facilisis ut arcu vel, ullamcorper commodo metus. Etiam malesuada erat massa, sit amet ornare nulla fringilla et. Nam eget velit congue, viverra dolor ac, lacinia justo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis feugiat arcu, non congue ex. Integer vel mollis leo.
+
+          </p>
+        </section>
+
+        <section>
+          <h2 className="text-header3 font-semibold mb-4">Contact Us</h2>
+          <p>
+            If you have any questions about this Privacy Notice or your data, please feel free to contact us at <a href="mailto:support@deliverydrone.com" className="text-blue-600 underline">support@dronedelivery.com</a>.
+          </p>
+        </section>
+
+        <div className="mt-12 text-center text-sm text-customGrey">
+          © {new Date().getFullYear()} DroneDelivery. All Rights Reserved.
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PrivacyNotice;
